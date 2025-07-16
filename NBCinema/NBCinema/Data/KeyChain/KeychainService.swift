188d9191e8cdb12e42fa2a3e055b20a97bcb9258
juseojo <j86371224@gmail.com>
2025-07-16 20:53:50 +0900

    Chore: 주석 추가하였습니다.

--- NBCinema/NBCinema/Data/KeyChain/KeychainService.swift
+++ NBCinema/NBCinema/Data/KeyChain/KeychainService.swift
@@ -10,6 +10,9 @@ import Security
 
 class KeychainService {
 
+	// MARK: - Save
+	// 성공시 true, 실패시 false 반환
+	// 결과값 사용하지 않아도 경고 안뜸
 	@discardableResult
 	static func save(service: String, account: String, value: String) -> Bool {
 		guard let valueData = value.data(using: .utf8) else { return false }
@@ -28,6 +31,8 @@ class KeychainService {
 		return status == errSecSuccess
 	}
 
+	// MARK: - Load
+	// 저장 되어있는 value값이 문자열로 반환됩니다.
 	static func load(service: String, account: String) -> String? {
 		let query: [CFString : Any] = [
 			kSecClass : kSecClassGenericPassword,
@@ -48,6 +53,9 @@ class KeychainService {
 		return result
 	}
 
+	// MARK: - Delete
+	// 성공시 true, 실패시 false 반환
+	// 결과값 사용하지 않아도 경고 안뜸
 	@discardableResult
 	static func delete(service: String, account: String) -> Bool {
 		let query: [CFString : Any] = [
